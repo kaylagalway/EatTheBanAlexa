59e83bb6d07cc3039fe582c2170681cf87ce1f9d
Cordavi <michaelamundsen@gmail.com>
2017-03-08 15:33:38 -0500

    Added in browseByCountry intent

--- src/TestingIndex.js
+++ src/TestingIndex.js
@@ -3,17 +3,19 @@ var Alexa = require('alexa-sdk');
 var APP_ID = "amzn1.ask.skill.045843cb-12ef-4af0-8371-3a650b6c5131";  // TODO replace with your app ID (OPTIONAL).
 
 var states = {
-    STARTMODE: '_STARTMODE',                // Prompt the user to start or restart the game.
-    ASKMODE: '_ASKMODE',                    // Alexa is asking user the questions.
-    DESCRIPTIONMODE: '_DESCRIPTIONMODE'     // Alexa is describing the final choice and prompting to start again or quit
+    STARTMODE: '_STARTMODE', 
+    BROWSEMODE: '_BROWSEMODE',               
+    ASKMODE: '_ASKMODE',                    
+    DESCRIPTIONMODE: '_DESCRIPTIONMODE'     
 };
 
 
 var allRestaurants = { 
-	"countries": [{
-		"name": "Iran",
-		"restaurants": [{
-			"name": "Ravagh Persian Grill",
+	"countries": [
+
+		{"name": "Iran",
+		"restaurants": [
+			{"name": "Ravagh Persian Grill",
 			"locations": [
 				"11 E 30th St, Manhattan", 
 				"1135 1st Ave, Manhattan", 
@@ -22,159 +24,125 @@ var allRestaurants = {
     		{"name": "Taste of Persia",
     		"locations": [
     			"12 W 18th St, Manhattan"
+    		]},
+    		{"name": "Pars Grill House",
+    		"locations": [
+    			"249 W 26th St, Manhattan"
+    		]},
+    		{"name": "Colbeh",
+    		"locations": [
+    			"75 N Station Plaza, Great Neck"
+    		]},
+    		{"name": "Persepolis",
+    		"locations": [
+    			"1407 2nd Ave, Manhattan"
+    		]},
+    		{"name": "Patoug Persian Cuisine",
+    		"locations": [
+    			"220-06 Horace Harding Expy, Queens"
+    		]},
+    		{"name": "Shiraz Restaurant",
+    		"locations": [
+    			"220-06 Horace Harding Expy, Queens"
+    		]},
+    		{"name": "Shiraz Kitchen",
+    		"locations": [
+    			"83 E Main St, Elmsford, NY"
+    		]},
+    		{"name": "Chatanooga Persian",
+    		"locations": [
+    			"37 Cutter Mill Rd, Great Neck"
+    		]},
+    		{"name": "Bijan's",
+    		"locations": [
+    			"81 Hoyt St, Brooklyn"
     		]}
-		]}
-	]
-}
+    	]},
 
-var countriesArray = [
-    {"Iran": [
-        {"Ravagh Persian Grill": {
-            "Locations": [
-                "11 E 30th St, Manhattan", 
-                "1135 1st Ave, Manhattan", 
-                "125 1st Ave, Manhattan"
-            ]
-        }},
-        {"Taste of Persia": {
-            "Locations": [
-                "12 W 18th St, Manhattan" 
-            ]
-        }},
-        {"Pars Grill House": {
-            "Locations": [
-                "249 W 26th St, Manhattan"
-            ]
-        }},
-        {"Colbeh": {
-            "Locations": [
-                "75 N Station Plaza, Great Neck"
-            ]
-        }},
-        {"Persepolis": {
-            "Locations": [
-                "1407 2nd Ave, Manhattan"
-            ]
-        }},
-        {"Patoug Persian Cuisine": {
-            "Locations": [
-                "220-06 Horace Harding Expy, Queens"
-            ]
-        }},
-        {"Shiraz Restaurant": {
-            "Locations": [
-                "770 Middle Neck Rd, Great Neck"
-            ]
-        }},
-        {"Shiraz Kitchen": {
-            "Locations": [
-                "83 E Main St, Elmsford, NY"
-            ]
-        }},
-        {"Chatanooga Persian": {
-            "Locations": [
-                "37 Cutter Mill Rd, Great Neck"
-                ]
-        }},
-        {"Bijan's": {
-            "Locations": [
-                "81 Hoyt St, Brooklyn"
-            ]
-        }}
-    ]},
-    {"Iraq": [
-        {"Moustache": {
-            "Locations": [
-                "90 Bedford St, Manhattan",
-                "265 E 10th St, Manhattan",
-                "1621 Lexington Ave, Manhattan"
-            ]
-        }}
-    ]},
-    {"Yemen": [
-        {"Yemen Cafe": {
-            "Locations": [
-                "176 Atlantic Ave, Brooklyn",
+    	{"name": "Iraq",
+    	"restaurants": [{
+    		"name": "Moustache",
+    		"locations": [
+    			"90 Bedford St, Manhattan",
+    			"265 E 10th St, Manhattan",
+    			"1621 Lexington Ave, Manhattan"
+    		]}
+    	]},
+
+    	{"name": "Yemen",
+    	"restaurants": [{
+    		"name": "Yemen Cafe",
+    		"locations": [
+    		     "176 Atlantic Ave, Brooklyn",
                  "7130 5th Ave, Brooklyn"
-            ]
-        }},
-        {"Yemen Cuisine": {
-            "Locations": [
-                "145 Court St, Brooklyn"
-            ]
-        }},
-        {"Hadramout": {
-            "Locations": [
-                "172 Atlantic Ave, Brooklyn"
-            ]
-        }},
-        {"Maya Cuisine": {
-            "Locations": [
-                "24-42 Steinway Street, Astoria"
-            ]
-        }},
-        {"Grill 212": {
-            "Locations": [
-                "212 W 80th St, Manhattan"
-            ]
-        }}
-    ]},
-    {"Somalia": [
-        {"Safari Restaurant": {
-            "Locations": [
-                "219 W 116th St, Manhattan"
-            ]
-        }}
-    ]},
-    {"Syria": [
-        {"Al Salam Restaurant": {
-            "Locations": [
-                "7206 5th Ave, Brooklyn"
-            ]
-        }},
-        {"Balady Foods": {
-            "Locations": [
-                "7128 5th Ave, Brooklyn"
-            ]
-        }},
-        {"Damascus Bakery": {
-            "Locations": [
-                "195 Atlantic Ave, Brooklyn"
-            ]
-        }},
-        {"Cafe Rakka": {
-            "Locations": [
-                "81 St. Marks Pl, Manhattan"
-            ]
-        }},
-        {"Aleppo": {
-            "Locations": [
-                "939 Main St, Paterson, NJ"
-            ]
-        }},
-        {"Mansoura": {
-            "Locations": [
-                "515 Kings Hwy, Brooklyn"
-            ]
-        }},
-        {"First Oasis": {
-            "Locations": [
-                "9218 4th Ave, Brooklyn"
-            ]
-        }},
-        {"Mar Mar": {
-            "Locations": [
-                "370 Forest Ave, Staten Island"
-            ]
-        }},
-        {"Syrian Sweet Refuge": {
-            "Locations": [
-                "Multiple Locations"
-            ]
-        }}
-    ]}
-];
+    		]},
+    		{"name": "Yemen Cuisine",
+    		"locations": [
+    		     "145 Court St, Brooklyn"
+    		]},
+    		{"name": "Hadramout",
+    		"locations": [
+    		     "172 Atlantic Ave, Brooklyn"
+    		]},
+    		{"name": "Maya Cuisine",
+    		"locations": [
+    		     "24-42 Steinway Street, Astoria"
+    		]},
+    		{"name": "Grill 212",
+    		"locations": [
+    		     "212 W 80th St, Manhattan"
+    		]}
+    	]},
 
+    	{"name": "Somalia",
+    	"restaurants": [{
+    		"name": "Safari Restaurant",
+    		"locations": [
+    			"219 W 116th St, Manhattan"
+    		]}
+    	]},
+
+    	{"name": "Syria",
+		"restaurants": [
+			{"name": "Al Salam Restaurant",
+			"locations": [
+				"7206 5th Ave, Brooklyn", 
+    		]},
+    		{"name": "Balady Foods",
+    		"locations": [
+    			"7128 5th Ave, Brooklyn"
+    		]},
+    		{"name": "Damascus Bakery",
+    		"locations": [
+    			"195 Atlantic Ave, Brooklyn"
+    		]},
+    		{"name": "Cafe Rakka",
+    		"locations": [
+    			"81 St. Marks Pl, Manhattan"
+    		]},
+    		{"name": "Aleppo",
+    		"locations": [
+    			"939 Main St, Paterson, NJ"
+    		]},
+    		{"name": "Mansoura",
+    		"locations": [
+    			"515 Kings Hwy, Brooklyn"
+    		]},
+    		{"name": "First Oasis",
+    		"locations": [
+    			"9218 4th Ave, Brooklyn"
+    		]},
+    		{"name": "Mar Mar",
+    		"locations": [
+    			"370 Forest Ave, Staten Island"
+    		]},
+    		{"name": "Syrian Sweet Refuge",
+    		"locations": [
+    			"Multiple Locations"
+    		]}
+    	]}
+	]
+};
 
 //string name of chosen country
 var chosenCountry;
@@ -196,6 +164,9 @@ var welcomeMessage = "Welcome to Eat the Ban. Want a suggestion or to browse by 
 // This is the message that is repeated if the response to the initial welcome message is not heard
 var repeatWelcomeMessage = "Say suggestion for a restaurant or browse to pick a country.";
 
+var browseByCountryMessage = "Would you like to browse Iran, Iraq, or more?";
+var browseByCountryMessageTwo = "How about Syria, Somalia, or Yemen?";
+
 // this is the message that is repeated if Alexa does not hear/understand the reponse to the welcome message
 var promptToStartMessage = "Say suggestion, browse, or cancel";
 
@@ -211,22 +182,28 @@ var helpMessage = "I can make a restaurant suggestion or you can tell me a count
 // This is the goodbye message when the user has asked to quit the game
 var goodbyeMessage = "Ok, see you soon!";
 
+var countryNameDict = {
+	"Iran" : 0,
+	"Iraq" : 1,
+	"Yemen": 2,
+	"Somalia": 3,
+	"Syria": 4
+};
+
 // --------------- Handlers -----------------------
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
-    // To enable string internationalization (i18n) features, set a resources object.
-    alexa.resources = countriesArray;
-    alexa.registerHandlers(newSessionHandlers, startHandlers);
+    alexa.registerHandlers(newSessionHandlers, startHandlers, browseHandlers);
     alexa.execute();
 };
 
 //set the state to Start and welcome user
 var newSessionHandlers = {
     'LaunchRequest': function() {
-        this.handler.state = states.STARTMODE
-        this.emit(":ask", welcomeMessage, repeatWelcomeMessage)
+        this.handler.state = states.STARTMODE;
+        this.emit(":ask", welcomeMessage, repeatWelcomeMessage);
     },
     'AMAZON.HelpIntent': function () {
         this.handler.state = states.STARTMODE;
@@ -236,7 +213,7 @@ var newSessionHandlers = {
         this.handler.state = states.STARTMODE;
         this.emit(':ask', promptToStartMessage, promptToStartMessage);
     }
-}
+};
 
 // --------------- Functions that control the skill's behavior -----------------------
 
@@ -244,6 +221,10 @@ var newSessionHandlers = {
 var startHandlers = Alexa.CreateStateHandler(states.STARTMODE, {
     'SuggestionIntent': function () {
         helper.giveRandomSuggestion(this);
+    },
+    'BrowseIntent': function () {
+    	this.handler.state = states.BROWSEMODE;
+    	this.emit(':ask', browseByCountryMessage, browseByCountryMessage);
     },
     'AMAZON.YesIntent': function () {
         helper.giveRandomSuggestion(this);
@@ -253,6 +234,27 @@ var startHandlers = Alexa.CreateStateHandler(states.STARTMODE, {
     }
 });
 
+var browseHandlers = Alexa.CreateStateHandler(states.BROWSEMODE, {
+	'IranIntent': function() {
+		helper.browseByCountry(this, "Iran");
+	},
+	'IraqIntent': function() {
+		helper.browseByCountry(this, "Iraq");
+	},
+	'SomaliaIntent': function() {
+		helper.browseByCountry(this, "Somalia");
+	},
+	'SyriaIntent': function() {
+		helper.browseByCountry(this, "Syria");
+	},
+	'YemenIntent': function() {
+		helper.browseByCountry(this, "Yemen");
+	},
+	'MoreIntent': function() {
+		this.emit(':ask', browseByCountryMessageTwo, browseByCountryMessageTwo);
+	}
+});
+
 // --------------- Helper Functions  -----------------------
 
 var helper = {
@@ -260,12 +262,48 @@ var helper = {
         //need to add check to make sure not the same as previous suggestion
 
         //Get country
-        var countryIndex = Math.floor(Math.random() * allRestaurants.length);
-        var randomCountryDict = allRestaurants[countryIndex];
-        var restaurants = randomCountryDict["restaurants"];
-        var resturantIndex = Math.floor(Math.random() * restaurants.length);
-        var restaurant = restaurants[restaurantIndex];
-        var speechOutput = "Your restaurant is " + restaurant["name"] + ". " + "It is located at " + restaurant["locations"].first() + ". Would you like a different suggestion?";
+        var countriesArray = allRestaurants.countries;
+        var countryIndex = Math.floor(Math.random() * countriesArray.length);
+        var randomCountryDict = countriesArray[countryIndex];
+
+        //Get Restaurant
+        var restaurantsArray = randomCountryDict.restaurants;
+        var restaurantIndex = Math.floor(Math.random() * restaurantsArray.length);
+        var restaurant = restaurantsArray[restaurantIndex];
+
+        var speechOutput = "Your restaurant is " + restaurant.name + "from the country " + randomCountryDict.name + ". " + "It is located at " + restaurant.locations[0] + ". Would you like a different suggestion?";
+        context.emit(":ask", speechOutput, speechOutput);
+    },
+    browseByCountry: function (context, country) {
+    	//need to add check to make sure not the same as previous suggestion
+
+    	//retrieves index value from country name dict based on country string parameter
+    	var countryIndex = countryNameDict[country];
+
+    	//all restaurants is dictionary
+    	//.countries is key to countries array
+    	var countriesArray = allRestaurants.countries;
+
+    	//country index is dictionary to specific country
+    	var countryDict = countriesArray[countryIndex];
+
+    	//.restaurants is array of restaurant dictionaries
+    	var restaurantsArray = countryDict.restaurants;
+
+    	//creates random number for index of restaurant
+    	var restaurantIndex = Math.floor(Math.random() * restaurantsArray.length);
+
+    	//restaurant in restaurantsArray
+    	var restaurant = restaurantsArray[restaurantIndex];
+
+    	//.name is key to restaurant dictionary
+    	var restaurantName = restaurant.name;
+
+    	var speechOutput = restaurantName;
+
+        //var speechOutput = "Your restaurant is " + restaurant.name + "from the country " + country + ". " + "It is located at " + restaurant.locations[0] + ". Would you like a different suggestion?";
         context.emit(":ask", speechOutput, speechOutput);
     }
+
+
 };
